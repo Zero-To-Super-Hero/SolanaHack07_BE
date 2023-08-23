3815d0f68378137947f2fa594abcabb30ee64a99
LamSamChan <chan94170@gmail.com>
2023-08-23 21:21:55 +0700

    oke

---
+++ off_chain/off_chain/Models/PaymentDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace off_chain.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public string Total { get; set; }
+        [ForeignKey("PaymentId")]
+        public string PaymentId { get; set; }
+        public Payment Payment { get; set; }
+    }
+}
